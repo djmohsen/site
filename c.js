8eb61c389d0989f2a8f7787a811f9b1a1a66955a
dj <dj.moh3n@gmail.com>
2025-03-05 14:32:48 -0800

    Create c.js

---
+++ c.js
@@ -0,0 +1,17 @@
+    <script>
+window.chatwootSettings = {"position":"right","type":"standard","launcherTitle":"گفتگو"};
+      (function(d,t) {
+        var BASE_URL="https://chat.cpanel98.ir";
+        var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=BASE_URL+"/packs/js/sdk.js";
+        g.defer = true;
+        g.async = true;
+        s.parentNode.insertBefore(g,s);
+        g.onload=function(){
+          window.chatwootSDK.run({
+            websiteToken: 'tHHgyPrXXa9yCVNvzZEe92JE',
+            baseUrl: BASE_URL
+          })
+        }
+      })(document,"script");
+    </script>
